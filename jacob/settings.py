516c4075c1f6c70fe99e20c92cdadfd56c6e46be
f-nagel-1 <59820109+f-nagel-1@users.noreply.github.com>
2021-11-24 09:18:13 +0200

    Update settings.py

--- jacob/settings.py
+++ jacob/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g2vq%m336&yx%axrr$r9$7w4(_bbb^g@ncu)e9hu_hp8rt9aw0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
